15276ec8d5492b6d7945f2974c431e45d2a929c4
allannad <65513146+allannad@users.noreply.github.com>
2020-06-09 20:51:00 -0400

    finished assignment

--- get_data.py
+++ get_data.py
@@ -1,19 +1,22 @@
 import requests
 import json
+import statistics
 
 print("REQUESTING SOME DATA FROM THE INTERNET...")
 
+###
+# Write a python program which issues a GET request for this product.json data, 
+# then prints the product's "name".
+###
 
-#which issues a GET request for this product.json data, then prints the product's "name".
 request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/2.json"
 response = requests.get(request_url)
-print(response.status_code)
-print(response.text)
+#print(response.status_code)
+#print(response.text)
 
 products = json.loads(response.text)
-
 product = products["name"]
-print(product)
+print("product", product)
 
 ###
 # Write a Python program which issues a GET request for this products.json data,
@@ -24,17 +27,20 @@ request_url2 = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/
 otherproducts = requests.get(request_url2)
 listprods = json.loads(otherproducts.text)
 
-print(listprods)
+#print(listprods)
 
 nameid = []
-
+#
+#for p in listprods:
+#    x = p["name"]
+#    y = p["id"]
+#    #z = zip(x,y)
+#    nameid.append(x)
+#    nameid.append(y)
 for p in listprods:
-    x = p["name"]
-    y = p["id"]
-    #z = zip(x,y)
-    nameid.append(x)
+    print(p["name"],p["id"])
 
-print(nameid)
+#print("nameid", nameid)
 
 ###
 # Write a Python program which issues a GET request for this gradebook.json data, 
@@ -45,16 +51,20 @@ request_url3 = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/
 grades = requests.get(request_url3)
 listgrades = json.loads(grades.text)
 
-print("listgrades:", listgrades)
-
 students = listgrades["students"]
-print(students)
+#print("students",students)
 
 grades = []
 
 for i in students:
     i["finalGrade"] = float(i["finalGrade"])
-    #students["finalGrade"] = float(students["finalGrade"])
-    grades.append(i)
+    grades.append(i["finalGrade"])
 
-print(grades)
+avg = statistics.mean(grades)
+mingrades = min(grades)
+maxgrades = max(grades)
+
+#print(grades)
+print("average", avg)
+print("minimum",mingrades)
+print("maximum", maxgrades)
